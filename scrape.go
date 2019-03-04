78965d2c6c5891ecb10f0aada4ae480e523a83b3
BeckhamL <beckham.lam12@gmail.com>
2019-03-03 22:55:40 -0500

    fixed scraper, need to find max of map

--- scrape.go
+++ scrape.go
@@ -10,20 +10,36 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// better algorithm, get the title, break the paragraph into sentences, if the sentence contains a word in the title, add it
 func main() {
 
 	c := colly.NewCollector()
 
 	c.OnHTML(".l-container", func(e *colly.HTMLElement) {
-		text := e.ChildText(".zn-body__paragraph")
-		parseString(text)
+
+		paragraph := e.ChildText(".zn-body__paragraph")
+		title := e.ChildText(".pg-headline")
+
+		if title != "" {
+
+			counter := len(strings.Fields(title))
+			titleWords := make([]string, counter)
+			for i, words := range strings.Fields(title) {
+				titleWords[i] = words
+			}
+		}
+
+		if paragraph != "" {
+			parseString(paragraph)
+		}
+
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://www.cnn.com/2019/03/03/politics/rand-paul-trump-national-emergency-declaration/index.html")
+	c.Visit("https://www.cnn.com/2019/03/03/us/tornadoes-alabama-georgia-wxc/index.html")
 }
 
 func parseString(text string) {
@@ -36,16 +52,15 @@ func parseString(text string) {
 	newString := reg.ReplaceAllString(text, " ")
 
 	WordCount(newString)
-	//fmt.Println(newString)
 }
 
-// Function to found words
+// Function to find words
 func WordCount(s string) {
 
 	words := strings.Fields(s)
 	m := make(map[string]int)
 	for _, word := range words {
-		m[word] += 1
+		m[word]++
 	}
 
 	sortMap(m)
@@ -53,13 +68,47 @@ func WordCount(s string) {
 
 func sortMap(myMap map[string]int) map[string]int {
 
-	keys := make([]string, 0, len(myMap))
-	for k := range myMap {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
+	fillerWords := []string{"the", "to", "of", "a", "in", "and", "were", "they", "that", "have",
+		"for", "been", "said", "but", "by", "is", "at", "how", "why", "many", "in", "on", "go", "of"}
 
-	fmt.Println(myMap)
+	type kv struct {
+		Key   string
+		Value int
+	}
+
+	var ss []kv
+
+	for k, v := range myMap {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	newMap := make(map[string]int)
+
+	for _, kv := range ss {
+
+		if stringInSlice(kv.Key, fillerWords) {
+			delete(myMap, kv.Key)
+		}
+
+		newMap[kv.Key] = kv.Value
+	}
+
+	for k, v := range newMap {
+		fmt.Println(k, v)
+	}
 
 	return myMap
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
