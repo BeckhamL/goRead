0275e930eb36b0d16a53f05d4796a72377a62667
BeckhamL <beckham.lam12@gmail.com>
2019-05-17 09:08:47 -0400

    styling changes for landing page, routes for BBC and CNN

--- data.go
+++ data.go
@@ -100,34 +100,3 @@ func topHeadlinesBBCNews() []Article {
 
 	return articles
 }
-
-// func sports() []Article {
-
-// 	c := newsapi.NewClient("42b47add38084c6c99964fe24dcf6740", newsapi.WithHTTPClient(http.DefaultClient))
-// 	sources, err := c.GetTopHeadlines(context.Background(), &newsapi.TopHeadlineParameters{
-// 		Category: "sports",
-// 		Country:  "us",
-// 	})
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	var articles = []Article{}
-
-// 	for _, s := range sources.Articles {
-
-// 		a := Article{
-// 			Title:    s.Title,
-// 			Author:   s.Author,
-// 			Desc:     s.Content,
-// 			URL:      s.URL,
-// 			URLImage: s.URLToImage,
-// 		}
-
-// 		articles = append(articles, a)
-
-// 	}
-
-// 	return articles
-// }
